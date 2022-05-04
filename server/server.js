efa5b4fc17161bb0d641d5277ce5ae69bfe01fd6
Chaitanya134 <chaitanyagiri2@gmail.com>
2022-05-04 10:32:16 +0530

    Hosting Check

--- server/server.js
+++ server/server.js
@@ -4,12 +4,8 @@ const { PORT } = process.env;
 
 const connectDatabase = require("./config/database");
 
-console.log("DATABASE URI:", process.env.DB_URI);
-
 // Connecting to database
 connectDatabase();
-
-console.log("CONNECTED TO DATABASE");
 
 app.listen(PORT, () => {
     console.log(`SERVER RUNNING ON PORT ${PORT}`);
